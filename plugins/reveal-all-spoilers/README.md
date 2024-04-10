0faeb0bbeb1172cfd0b052fd1f2e6a0a26f6fc4b
12944qwerty <qwerty12944qwerty@gmail.com>
2024-04-09 21:07:35 -0400

    reveal all spoilers plugin

---
+++ plugins/reveal-all-spoilers/README.md
@@ -0,0 +1,9 @@
+# Reveal All Spoilers
+
+[![Install in Replugged](https://img.shields.io/badge/-Install%20in%20Replugged-blue?style=for-the-badge&logo=none)](https://replugged.dev/install?identifier=dev.kingfish.RevealAllSpoilers)
+
+Reveals all spoilers on a message when you ctrl+click on it. Cmd for Mac users.
+
+Port of [the bd version](https://betterdiscord.app/plugin/RevealAllSpoilers)
+
+![reveal all spoilers preview](/plugins/reveal-all-spoilers/assets/reveal.gif)
---
+++ plugins/reveal-all-spoilers/src/index.ts
@@ -0,0 +1,47 @@
+import { Injector } from "replugged";
+import { filters, waitForModule } from "replugged/webpack";
+
+const inject = new Injector();
+
+interface Spoiler {
+  default: {
+    prototype: {
+      renderWithTooltip: (args: {
+        props: {
+          children: (r: unknown) => React.ReactElement;
+        };
+      }) => unknown;
+    };
+  };
+}
+
+export async function start(): Promise<void> {
+  const Spoiler = await waitForModule<Spoiler>(filters.bySource("renderObscuredText"));
+
+  inject.before(Spoiler.default.prototype, "renderWithTooltip", ([ args ]) => {
+    const orig = args.props.children;
+    args.props.children = (r: unknown) => {
+      const res = orig(r);
+
+      const { onClick } = res.props;
+      if (!onClick) return res;
+
+      res.props.onClick = (e: MouseEvent) => {
+        if (e.metaKey || e.ctrlKey) {
+          const { target } = e;
+          if (!target) return;
+          (target as HTMLElement).parentNode
+            ?.querySelectorAll(".spoilerContent__383f3")
+            .forEach((e) => (e as HTMLElement).click());
+        }
+        onClick(e);
+      };
+
+      return res;
+    };
+  });
+}
+
+export function stop(): void {
+  inject.uninjectAll();
+}
