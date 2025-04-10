a08d36dd8f7b8fefccbecbc045305071e7e44f1f
12944qwerty <qwerty12944qwerty@gmail.com>
2025-04-10 16:39:54 -0400

    actually fix nsfw tag

--- plugins/better-nsfw-tag/src/index.tsx
+++ plugins/better-nsfw-tag/src/index.tsx
@@ -2,58 +2,70 @@ import type { Channel } from "discord-types/general";
 import type React from "react";
 import { Injector, Logger } from "replugged";
 import { Tree, findInReactTree } from "replugged/util";
-import {
-  filters,
-  getFunctionBySource,
-  getFunctionKeyBySource,
-  waitForModule,
-} from "replugged/webpack";
+import { filters, getFunctionKeyBySource, waitForModule, waitForProps } from "replugged/webpack";
 
 const inject = new Injector();
 const logger = Logger.plugin("BetterNSFWTag");
 
 interface ChannelItemModule {
-  default: React.FC<{ channel: Channel }>;
+  render: React.FC<{ channel: Channel }>;
+  [key: string]: unknown;
 }
-
-interface TextBadgeProps {
-  text: string;
-  style: React.CSSProperties;
+interface ChannelItemModuleAH {
+  render?: React.FC<{ channel: Channel }>;
+  [key: string]: unknown;
 }
 
 export async function start(): Promise<void> {
-  const mod = await waitForModule<ChannelItemModule>(filters.bySource(".unreadImportant:"));
-  const key = getFunctionKeyBySource(mod, ".unreadImportant:");
-
-  if (!key) {
+  const mod = await waitForModule<Record<string, ChannelItemModuleAH> | null>(filters.bySource(".unreadImportant:"));
+  if (typeof mod !== "object" || mod === null) {
     logger.error("Couldn't find the correct module to inject into.");
     return;
   }
 
-  const badgesMod = await waitForModule(filters.bySource(".textBadge,"));
-  const TextBadge = getFunctionBySource<React.FC<TextBadgeProps>>(badgesMod, "textBadge");
+  let obj;
+  for (const k in mod) {
+    try {
+      const v = mod[k];
+      if (typeof v === "object") {
+        if (v.render) {
+          obj = v;
+          break;
+        }
+      }
+    } catch {}
+  }
 
-  if (!TextBadge) {
-    logger.error("Couldn't find the TextBadge component.");
+  if (!obj) {
+    logger.error("Couldn't find the correct module to inject into.");
     return;
   }
 
-  inject.after(mod, key, ([{ channel }], res) => {
-    if (!channel.nsfw) return;
+  const badgeClasses = await waitForProps<Record<"textBadge" | "numberBadge", string>>("textBadge", "numberBadge");
+  const { mentionsBadge } = await waitForProps<Record<"mentionsBadge", string>>("mentionsBadge");
 
-    const badge = findInReactTree(
-      res as unknown as Tree,
-      (r) => Boolean(r?.className) && (r?.className as string).includes("linkTop"),
-    );
+  if (obj.render) {
+    inject.after(obj as ChannelItemModule, "render", ([{ channel }], res) => {
+      if (!channel.nsfw) return;
 
-    if (!badge) return;
+      const badge = findInReactTree(
+        res as unknown as Tree,
+        (r) => Boolean(r?.className) && (r?.className as string).includes("linkTop"),
+      );
 
-    (badge.children as JSX.Element[]).splice(
-      2,
-      0,
-      <TextBadge text="NSFW" style={{ borderRadius: "3px" }} />,
-    );
-  });
+      if (!badge) return;
+
+      (badge.children as JSX.Element[]).splice(
+        2,
+        0,
+        <div className={mentionsBadge}>
+          <div className={badgeClasses.textBadge} style={{backgroundColor: "var(--status-danger)"}}>
+            NSFW
+          </div>
+        </div>,
+      );
+    });
+  }
 }
 
 export function stop(): void {
